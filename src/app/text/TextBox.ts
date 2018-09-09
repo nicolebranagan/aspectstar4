d30fcaa6fbe2b795da8039fc6534c118d37125af
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2018-09-09 11:27:21 -0400

    Change font to use Chicago for readability

--- src/app/text/TextBox.ts
+++ src/app/text/TextBox.ts
@@ -13,11 +13,22 @@ enum TextBoxState {
 const TEXT_BOX_WIDTH = 398;
 const TEXT_BOX_HEIGHT = 99;
 
-const DEFAULT_TEXT_STYLE = new PIXI.TextStyle({
-    fontFamily: 'digitalDisco',
+const DEFAULT_TITLE_STYLE = new PIXI.TextStyle({
+    fontFamily: 'chicago',
     fontSize: 16,
     fontWeight: '100',
     fill: 'white'
+});
+
+const DEFAULT_TEXT_STYLE = new PIXI.TextStyle({
+    fontFamily: 'chicago',
+    fontSize: 14,
+    fontWeight: '100',
+    fill: 0xEEEEFF,
+    dropShadow: true,
+    dropShadowColor: 0x000000,
+    dropShadowAlpha: 0.7,
+    dropShadowDistance: 2,
 });
 
 export default class TextBox implements Runner {
@@ -43,11 +54,35 @@ export default class TextBox implements Runner {
         this.openTextBox();
     }
 
+    resetGraphics() {
+        if (this.graphics) {
+            this.drawables.removeChild(this.graphics);
+        }
+        this.graphics = new PIXI.Graphics();
+        this.graphics.x = 1;
+        this.graphics.y = 125;
+        this.graphics.width = TEXT_BOX_WIDTH;
+        this.graphics.height = TEXT_BOX_HEIGHT;
+        this.drawables.addChild(this.graphics);
+    }
+
     drawTextBox(height : number, width : number) {
         this.graphics.lineStyle(1, 0x777777);
         this.graphics.beginFill(0x001177);
         this.graphics.drawRect(0, 0, width, height);
         this.graphics.endFill();
+    }
+
+    drawChildTextBox(name : string) {
+        const metrics = PIXI.TextMetrics.measureText(name, DEFAULT_TITLE_STYLE);
+        this.graphics.lineStyle(1, 0x777777);
+        this.graphics.beginFill(0x000022);
+        this.graphics.drawRoundedRect(0, -10, metrics.width+32, 24, 14);
+        this.graphics.endFill();
+        const nameText = new PIXI.Text(name, DEFAULT_TITLE_STYLE);
+        nameText.x = 16;
+        nameText.y = 118;
+        this.drawables.addChild(nameText)
     }
 
     openTextBox() {
@@ -63,9 +98,12 @@ export default class TextBox implements Runner {
     }
 
     drawText() {
-        const text = new PIXI.Text('Welcome to Aspect Star 4!\nChoosing fonts is hard...', DEFAULT_TEXT_STYLE);
-        text.x = 32 + 5;
-        text.y = 130;
+        this.resetGraphics();
+        this.drawTextBox(TEXT_BOX_HEIGHT, TEXT_BOX_WIDTH);
+        this.drawChildTextBox('Nicole');
+        const text = new PIXI.Text('Welcome to Aspect Star 4!\nChoosing fonts is hard...\nSome more newlines...\nSo hard for it, honey\nSo hard for it, honey', DEFAULT_TEXT_STYLE);
+        text.x = 8;
+        text.y = 140;
         this.drawables.addChild(text)
     }
 
