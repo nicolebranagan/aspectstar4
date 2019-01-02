ac9f6078f87d5d02207bf8a62b9fa38cfd2c9f92
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2019-01-02 16:46:47 -0500

    A big spinning sun

--- src/app/level/backgrounds/Vaporcity.ts
+++ src/app/level/backgrounds/Vaporcity.ts
@@ -1,9 +1,23 @@
 import Background from "../../interfaces/Background";
 
+const FALSE_SUNRISE = [0, 144, 72, 40];
+
+const generateSprite = (rect : number[], x : number, y : number) => {
+    const texture = PIXI.Texture.from(PIXI.loader.resources['background'].texture.baseTexture); 
+    const pixiRect = new PIXI.Rectangle(...rect);
+    texture.frame = pixiRect;       
+    const sprite = new PIXI.Sprite()
+    sprite.texture = texture;
+    sprite.x = x;
+    sprite.y = y;
+    return sprite;
+};
+
 export default class Vaporcity implements Background {
     public drawables : PIXI.Container;
 
     private canvas : HTMLCanvasElement;
+    private falseSunrise : PIXI.Sprite;
 
     constructor() {
         this.drawables = new PIXI.Container;
@@ -13,7 +27,7 @@ export default class Vaporcity implements Background {
     }
 
     updatePos(x : number, y : number, offset : number) {
-
+        this.falseSunrise.x = ((Math.abs(x) + 172 + 72) % 544) - 72;
     }
 
     private prepareCanvas() {
@@ -37,5 +51,8 @@ export default class Vaporcity implements Background {
         sprite.y = 0;
         sprite.anchor.set(0,0);
         this.drawables.addChild(sprite);
+
+        this.falseSunrise = generateSprite(FALSE_SUNRISE, 172, 90-40)
+        this.drawables.addChild(this.falseSunrise);
     }
 }
